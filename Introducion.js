1d7e1bd3b1bd2d02fe74c0976b2ca0208bfe6db8
Miguel Angel Jimenez Melendez <miguejm@outlook.es>
2020-08-01 15:56:58 -0500

    numeros y operaciones aritmeticas

--- Introducion.js
+++ Introducion.js
@@ -12,3 +12,10 @@ let nombre = "Miguel";
 var apellido = "Jimenez";
 //Constantes  -- JavaScript es un lenguaje tipado, para declarar varibles sigue un estandar
 const PI = 3.1416;
+
+//Numeros y operaciones aritmeticas
+// libreria/biblioteca: Codigo que otros desarrolladores comparten con los demas.
+Math.PI;
+Math.pow(10, 2);
+Math.round(6.5);
+Math.sqrt(4); // https://www.w3schools.com/js/js_math.asp
