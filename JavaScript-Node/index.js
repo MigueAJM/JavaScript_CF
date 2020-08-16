e30a7aae3951515c463ff71f1ec6d5ba44b873f4
MiGueAJM9724 <miguejm@outlook.es>
2020-08-16 17:02:44 -0500

    Crear promesas

--- JavaScript-Node/index.js
+++ JavaScript-Node/index.js
@@ -23,3 +23,35 @@ rp('https://google.com').then(function(html){
 * Pending
 * Setled
 */
+
+// Crear mis propias Promesas
+function asincrono(){
+  return new Promise(function(resolve, reject){
+    resolve('Todo salio bien');
+
+    reject(new Error('No se pudo completar'));
+  });
+}
+
+const request = require('request');
+
+function leer(url){
+  return new Promise(function(resolve, reject){
+    // metodo asincrono
+    request(url, function(err, response){
+      if(err) {
+        reject(err);
+      }else{
+        resolve(response);
+      }
+    });
+  });
+}
+
+leer('https://codigofacilito.com')
+  .then(function(response){
+    console.log(response);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
