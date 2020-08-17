7ca55223730252846d4780bdd7824890dd4ed931
MiGueAJM9724 <miguejm@outlook.es>
2020-08-17 17:31:40 -0500

    readOnly Imports

--- modulos/modulo_base.js
+++ modulos/modulo_base.js
@@ -1,6 +1,6 @@
 //import { name } from './modulo_dos.mjs'
 
-import def, {name, edad} from './modulo_dos.mjs'
+import def, {name, edad} from './modulo_dos.mjs' // def, {name, edad} son valores readOnly imports
 //console.log(name);
 console.log(def, nme, edad);
 //para instalar servidor local usando node
