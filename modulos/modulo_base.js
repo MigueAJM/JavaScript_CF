71696b45f001b42e46c0958cc37cd2765210c8ae
MiGueAJM9724 <miguejm@outlook.es>
2020-08-17 17:25:46 -0500

    Valores por defecto

--- modulos/modulo_base.js
+++ modulos/modulo_base.js
@@ -1,6 +1,8 @@
-import { name } from './modulo_dos.mjs'
-console.log(name);
+//import { name } from './modulo_dos.mjs'
 
+import def, {name, edad} from './modulo_dos.mjs'
+//console.log(name);
+console.log(def, nme, edad);
 //para instalar servidor local usando node
 // ejecutar la siguiente instruccion en la terminal, en la carpeta donde tengas alojado tu proyecto
 // npm install -g node-static
--- modulos/modulo_dos.mjs
+++ modulos/modulo_dos.mjs
@@ -1,2 +1,6 @@
 //se recomienda usar .mjs para los Modulos
 export const name = "Migue";
+export const edad = 25;
+
+let valorPorDefecto = 20;
+export default valorPorDefecto;
