afcc791ffdb537c620e8e67d18a4aaf57d046bfc
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 10:30:55 -0500

    Iterables con iteradores

--- GeneradoresIterador/iterador.js
+++ GeneradoresIterador/iterador.js
@@ -102,3 +102,65 @@ obj[simbolo] = function(){
 }
 
 obj[simbolo]();
+
+
+// Iterables con iteradores
+function* counter(){
+  for(var i = 1; i<=5; i++){
+    yield i;
+  }
+}
+let generador = counter();
+
+let numeros = [1, 2, 4];
+for(numero of generador){ console.log(numero); }
+
+//@@iterator
+//symbol.iterator
+let contador = {
+  [Symbol.iterator](){
+    return {
+      currentValue: 1,
+      next(){
+        let resultado = {
+          value: null,
+          done: false
+        };
+        if(this.currentValue > 0 && this.currentValue <= 5){
+          resultado = { value: this.currentValue, done: false};
+          this.currentValue += 1;
+        }else{
+          result = {done: true};
+        }
+        return resultado;
+      }
+    };
+  }
+}
+
+for(numero of contador){ console.log(numero); }
+
+let rango = {
+  min: null,
+  max: null,
+  currentValue: null,
+  [Symbol.iterator](){
+    return this;
+  },
+  next(){
+    if(this.currentValue == null) this.currentValue = this.min;
+    let result = {};
+    if(this.currentValue >= this.min && this.currentValue <= this.max){
+      result = { value: this.currentValue, done: false };
+      this.currentValue += 1;
+    }else{
+      result = { done: true };
+    }
+    return result;
+  }
+};
+
+rango.min = 5;
+rango.max = 10;
+
+for(n of rango){ console.log(n); }
