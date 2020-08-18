a6fe8c39d8d8d9f4825658eb0d5e3542d942d62b
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 09:51:39 -0500

    return en funciones generadoras

--- GeneradoresIterador/iterador.js
+++ GeneradoresIterador/iterador.js
@@ -21,3 +21,22 @@ while(!item.done){
   console.log(item.value);
   item = iterador.next();
 }
+
+//Return en funciones generadoras
+// yield es muy parecido a return
+function* returnador(){
+  return 3;
+}
+
+let g = returnador();
+console.log(g.next());
+
+function* counter(){
+  for(var i = 1; i<=5; i++){
+    yield i;
+  }
+}
+let generador = counter();
+console.log(generador.next());
+console.log(generador.next());
+console.log(generador.next());
