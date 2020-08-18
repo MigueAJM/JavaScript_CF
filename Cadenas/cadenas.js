76a5cc9ed9e352fdb8c7a9f2a98faf6f11a2b92c
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 17:26:55 -0500

    definir cadenas

---
+++ Cadenas/cadenas.js
@@ -0,0 +1,10 @@
+// Como definir cadenas existen dos versiones de las cadenas
+//primitivo ""  ''
+let nombre = "migue";
+console.log(nombre.length);
+//objeto
+let name = new String("Migue");
+console.log(typeof nombre);
+console.log(typeof name);
+
+//    ----  Cuarsión de tipos ----
