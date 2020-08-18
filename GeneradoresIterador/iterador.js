e675878e31c297a1fa1c97f7fe24c427607f4d8a
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 10:36:04 -0500

    iterables y generadores

--- GeneradoresIterador/iterador.js
+++ GeneradoresIterador/iterador.js
@@ -164,3 +164,20 @@ rango.min = 5;
 rango.max = 10;
 
 for(n of rango){ console.log(n); }
+
+// iterables y Generadores
+let rango = {
+  min: null,
+  max: null,
+  [Symbol.iterator](){
+    return this.generador();
+  },
+  generador: function* (){
+    for(var i = this.min; i <= this.max; i++ ){
+      yield i;
+    }
+  }
+};
+rango.min = 0;
+rango.max = 100;
+for(n of rango){ console.log(n); }
