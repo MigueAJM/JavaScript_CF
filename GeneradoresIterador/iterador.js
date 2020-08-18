8c73c320fec8bfbfb4c97e73225c87541c006afe
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 10:11:42 -0500

    Manejo de simbolos

--- GeneradoresIterador/iterador.js
+++ GeneradoresIterador/iterador.js
@@ -86,3 +86,19 @@ console.log(g.next());
 console.log(g.next());
 console.log(g.next());
 console.log(g.next());
+
+
+//  Manejo de simbolos
+let simbolo = Symbol('mi-simbolo');
+let simbolo2 = Symbol('mi-simbolo');
+console.log(simbolo == simbolo2);
+
+let obj = {
+
+};
+
+obj[simbolo] = function(){
+  console.log("Mi nombre es un simbolo");
+}
+
+obj[simbolo]();
