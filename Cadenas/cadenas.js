77b4ebb94306fb552b7e3f67cea6cb49633a13ad
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 17:55:55 -0500

    Concatenación e interpolación

--- Cadenas/cadenas.js
+++ Cadenas/cadenas.js
@@ -13,3 +13,23 @@ console.log(typeof name);
 // caracteres especiales
 // escaping => identificación de caracteres especiales
 console.log("Migue dijo: \"Hola\"");
+
+//Concatenación e interpretación
+console.log("Hola" + " mundo");
+let a = "Hola";
+let b = "Mundo";
+console.log(a.concact(b));
+//template litrals => template string
+let nombre = "Migue";
+let template = `Hola ${nombre}, ¿Como estäs?`;
+console.log(template);
+
+//1-9 => 0n
+//11-12 => 11 12
+let mes = 3;
+console.log(mes.padStart(2,"0"));
+let mes = prompt("Ingresa mes de nacimiento");
+if(mes<10){
+   mes = `0${mes}`;
+}
+console.log(mes);
