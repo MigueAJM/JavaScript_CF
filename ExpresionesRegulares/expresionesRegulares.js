1e80561e1f8e7b7630717b0a1e91aa8d76f02b8f
MiGueAJM9724 <miguejm@outlook.es>
2020-08-18 20:47:53 -0500

    Expresiones regulares

---
+++ ExpresionesRegulares/expresionesRegulares.js
@@ -0,0 +1,6 @@
+//nos permiten trabajar con patrones de busqueda sobre el texto, basada en patrones
+//dos alternativas  /Expresión/;  new RegExp()
+// Literales, especiales (caracteres especiales (meta caracteres) se agrupan en 5 grupos)
+//tipo caracter, las acerciones, agrupamientos/rangos, cuantificadores, unidcode property escapes
+/.@./ //Cualquier caracter antes de @ y despues de arroba   ||  regex101.com
+/.@./g
